ce8099db58a5882f75b877a6d150e08e7e0b9ee5
Olga Paoletti <olgpao@arsanita.toscana.it>
2023-04-04 12:15:28 +0200

    Update of the example 1

--- examples/example_01/to_run_CreateDoT_example_01.R
+++ examples/example_01/to_run_CreateDoT_example_01.R
@@ -27,18 +27,20 @@ input <-as.data.table(read_excel(paste0(thisdir,"/data/input.xlsx")))
 
 output <- CreateDOT(dataframe = input, 
                     recipe = "Units of presentations per day", 
-                    output_var = "DOT_recipe_1", #default days_of_treatment
+                    output_var = "CALCULATED_DoT",   #default days_of_treatment
                     disp_num_medicinal_product = "disp_num_medicinal_product",  
                     unit_of_presentation_num = "unit_of_presentation_num", 
-                    presc_quantity_per_day = "dd", 
                     subst_amount_per_form_subst1 = "subst1_amount_per_form",
                     subst_amount_per_form_subst1_unit = "subst1_amount_unit",
                     concentration_subst1 = "subst1_concentration",
                     concentration_subst1_unit = "subst1_concentration_unit",
                     concentration_total_content= "concentration_total_content",
                     dd="dd",
-                    dd_unit = "unit_dd"
+                    dd_unit = "unit_dd",
+                    output_dd1="CALCULATE_DD_subst1",
+                    output_dd1_unit="CALCULATE_DD_subst1_unit"
                     )  
 
 View(output)
+DOT_recipe_1
 fwrite(output,file=paste0(thisdir,"/data/output.csv"))
